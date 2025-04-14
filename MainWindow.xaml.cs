96851ecbf2f3b2b244443c191903b955d08c0b9a
Marcin Kowalski <m.kowalski.nov7@outlook.com>
2025-04-14 14:42:29 +0200

    Load csv files

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System;
 
 namespace ListViewApp
 {
@@ -23,11 +27,45 @@ namespace ListViewApp
 
         private void New_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mainList.Items.Clear();
+                string filePath = openFileDialog.FileName;
+                int selectedFilterIndex = openFileDialog.FilterIndex;
+                string delimiter = ";";
+                if (selectedFilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                Encoding encoding = Encoding.UTF8;
+
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadAllLines(filePath,encoding);
+                    foreach (var line in lines)
+                    {
+                        var columns = line.Split(delimiter);
+
+
+                        mainList.Items.Add(new
+                        {
+                            m_strPESEL = columns.ElementAtOrDefault(0),
+                            m_strName = columns.ElementAtOrDefault(1),
+                            m_strSecName = columns.ElementAtOrDefault(2),
+                            m_strSurname = columns.ElementAtOrDefault(3)
+                        });
+
+
+                    }
+                }
+            }
 
         }
 
